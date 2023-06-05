b3d4f7c8f647f125a78b4892d4d09d3c3fdad465
Jeffrey Kern <jeffkernsemail@gmail.com>
2023-06-05 16:18:31 -0500

    finished up steps 1-4

---
+++ my-app/src/components/GoodnightMoon/GoodnightMoon.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+interface GoodnightMoonProps {}
+
+const GoodnightMoon: FC<GoodnightMoonProps> = () => (
+  <div>
+    GoodnightMoon Component
+  </div>
+);
+
+export default GoodnightMoon;
