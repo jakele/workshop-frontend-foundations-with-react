8855681f9cd706409782e2dfab7fa4dd341f8dc1
Jeffrey Kern <jeffkernsemail@gmail.com>
2023-06-05 16:29:43 -0500

    working routing

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,30 +1,21 @@
-import './App.css';
+import "./App.css";
 
-import GoodnightMoon from './components/GoodnightMoon/GoodnightMoon';
-import HelloWorld from './HelloWorld';
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import GoodnightMoon from "./components/GoodnightMoon/GoodnightMoon";
+import HelloWorld from "./HelloWorld";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <HelloWorld></HelloWorld>
-        <GoodnightMoon></GoodnightMoon>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HelloWorld />}></Route>
+          <Route path="/goodnightmoon" element={<GoodnightMoon />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
