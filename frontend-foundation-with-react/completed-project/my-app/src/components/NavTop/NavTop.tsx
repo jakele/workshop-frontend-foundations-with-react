a43d9ac32cacd941e0aecf29083c5cd1ec9c1b90
Jeffrey Kern <jeffkernsemail@gmail.com>
2023-06-06 11:25:02 -0500

    updating finished nav

--- frontend-foundation-with-react/completed-project/my-app/src/components/NavTop/NavTop.tsx
+++ frontend-foundation-with-react/completed-project/my-app/src/components/NavTop/NavTop.tsx
@@ -1,12 +1,7 @@
-import * as React from 'react';
-
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 
 export default function NavTop() {
   return (
